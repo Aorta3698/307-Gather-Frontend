26445e36918f2d05a7d2ca8b3fa0d219d9eb07dc
Ash <86912625+anarchaworld@users.noreply.github.com>
2023-03-13 13:43:08 -0700

    home now shows info from backend

--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,3 @@
-// import React, { useState, useEffect } from "react";
-// import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
-import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Button from "@mui/material/Button";
 import GroupsIcon from "@mui/icons-material/Groups";
@@ -18,7 +15,9 @@ import Link from "@mui/material/Link";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
 import Map from "./Map";
-import { getAllEvents } from "./Events";
+
+import React, {Component} from "react";
+import {getAllEvents} from "./Events";
 
 function Copyright() {
   return (
@@ -33,117 +32,120 @@ function Copyright() {
   );
 }
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// const cards = getAllEvents();
-// console.log(getAllEvents());
-// const arr = getAllEvents().then((res) => res.data);
-// console.log(arr);
-
 const theme = createTheme({
   palette: {
     mode: "dark",
   },
 });
 
-export default function Home() {
-  return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <AppBar position="relative">
-        {/* Header */}
-        <Toolbar>
-          <GroupsIcon sx={{ mr: 2 }} />
-          <Typography variant="h6" color="inherit" noWrap>
-            Gather
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <main>
-        {/* Hero unit */}
-        <Box
-          sx={{
-            bgcolor: "background.paper",
-            pt: 8,
-            pb: 6,
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography
-              component="h1"
-              variant="h2"
-              align="center"
-              color="text.primary"
-              gutterBottom
-            >
+export default class Home extends Component {
+  state = {events: []};
+
+  componentDidMount() {
+    getAllEvents().then(data => {
+      const events = data;
+      this.setState({events});
+    })
+  }
+
+  render(){
+    return (
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="relative">
+          {/* Header */}
+          <Toolbar>
+            <GroupsIcon sx={{ mr: 2 }} />
+            <Typography variant="h6" color="inherit" noWrap>
               Gather
             </Typography>
-            <Typography
-              variant="h5"
-              align="center"
-              color="text.secondary"
-              paragraph
-            >
-              This is the place where you can find events to attend, host new
-              events, and meet new people.
-            </Typography>
-          </Container>
-        </Box>
+          </Toolbar>
+        </AppBar>
+        <main>
+          {/* Hero unit */}
+          <Box
+            sx={{
+              bgcolor: "background.paper",
+              pt: 8,
+              pb: 6,
+            }}
+          >
+            <Container maxWidth="sm">
+              <Typography
+                component="h1"
+                variant="h2"
+                align="center"
+                color="text.primary"
+                gutterBottom
+              >
+                Gather
+              </Typography>
+              <Typography
+                variant="h5"
+                align="center"
+                color="text.secondary"
+                paragraph
+              >
+                This is the place where you can find events to attend, host new
+                events, and meet new people.
+              </Typography>
+            </Container>
+          </Box>
 
-        {/* Map */}
-        <Map />
+          {/* Map */}
+          <Map />
 
-        {/* Event Cards */}
-        <Container sx={{ py: 5 }} maxWidth="md">
-          <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card
-                  sx={{
-                    height: "100%",
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <CardMedia
-                    component="img"
+          {/* Event Cards */}
+          <Container sx={{ py: 5 }} maxWidth="md">
+            <Grid container spacing={4}>
+              {this.state.events.map((event, index) => (
+                <Grid item key={index} xs={12} sm={6} md={4}>
+                  <Card
                     sx={{
-                      // 16:9
-                      pt: "56.25%",
+                      height: "100%",
+                      display: "flex",
+                      flexDirection: "column",
                     }}
-                    image="images/event.png"
-                    alt="event discription"
-                  />
-                  <CardContent sx={{ flexGrow: 1 }}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      card.name
-                    </Typography>
-                    <Typography>card.description</Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" variant="outlined">
-                      View
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
-            ))}
-          </Grid>
-        </Container>
-      </main>
+                  >
+                    <CardMedia
+                      component="img"
+                      sx={{
+                        // 16:9
+                        pt: "56.25%",
+                      }}
+                      image="images/event.png"
+                      alt="event discription"
+                    />
+                    <CardContent sx={{ flexGrow: 1 }}>
+                      <Typography gutterBottom variant="h5" component="h2">
+                        {event.name}
+                      </Typography>
+                      <Typography>{event.description}</Typography>
+                    </CardContent>
+                    <CardActions>
+                      <Button size="small" variant="outlined">
+                        View
+                      </Button>
+                    </CardActions>
+                  </Card>
+                </Grid>
+              ))}
+            </Grid>
+          </Container>
+        </main>
 
-      {/* Footer */}
-      <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
-        <Typography
-          variant="subtitle1"
-          align="center"
-          color="text.secondary"
-          component="p"
-        >
-          Made by Cal Poly students for Cal Poly SLO in CSC 307.
-        </Typography>
-        <Copyright />
-      </Box>
-    </ThemeProvider>
-  );
-}
+        {/* Footer */}
+        <Box sx={{ bgcolor: "background.paper", p: 6 }} component="footer">
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+          >
+            Made by Cal Poly students for Cal Poly SLO in CSC 307.
+          </Typography>
+          <Copyright />
+        </Box>
+      </ThemeProvider>
+    );
+  }}
