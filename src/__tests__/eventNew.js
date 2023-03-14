6b2d5d05ea90d3b83d8e45829db25dfcf145df1c
anarchaworld <anarchaworld@gmail.com>
2023-03-14 14:52:27 -0700

    added a test case for loading new event page

---
+++ src/__tests__/eventNew.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import EventNew from "../EventNew";
+
+it("New event page renders without crashing.", () => {
+  const div = document.createElement("div");
+  ReactDOM.createRoot(div).render(<EventNew />);
+});
