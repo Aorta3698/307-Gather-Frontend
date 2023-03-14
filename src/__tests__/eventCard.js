157672fd2427de98298bf56debd9ae7e0dc30735
anarchaworld <anarchaworld@gmail.com>
2023-03-14 14:52:47 -0700

    added a test case for loading event cards

---
+++ src/__tests__/eventCard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import EventCards from "../EventCards";
+
+it("Event Cards renders without crashing.", () => {
+  const div = document.createElement("div");
+  ReactDOM.createRoot(div).render(<EventCards />);
+});
