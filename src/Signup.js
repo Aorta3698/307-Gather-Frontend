da93f93a913d8edc82bfbdc02dfd53a6bd2b810f
anarchaworld <anarchaworld@gmail.com>
2023-03-14 15:08:06 -0700

    fixed a bug + cleaned it up

--- src/Signup.js
+++ src/Signup.js
@@ -1,21 +1,21 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 
-export default function Signup(props) {
-  const navigate = useNavigate(); 
+import Copyright from "./Copyright";
+
+export default function Signup() {
+  const navigate = useNavigate();
   const [person, setPerson] = useState({
     firstname: "",
     lastname: "",
@@ -56,46 +56,40 @@ export default function Signup(props) {
   }
 
   function submitForm() {
-    console.log("Submit Form Person: " + person);
-    //props.handleSubmit(person);
     setPerson({ firstname: "", lastname: "", email: "", password: "" });
     navigate("/home");
   }
 
-  function Copyright(props) {
-    return (
-      <Typography variant="body2" color="text.secondary" align="center" {...props}>
-        {'Copyright © '}
-        <Link color="inherit" href="https://gather-5411b.web.app/">
-          Gather
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
-      </Typography>
-    );
-  }
-  
-  const theme = createTheme();
+  const theme = createTheme({
+    palette: {
+      mode: "dark",
+    },
+  });
 
-  return(
+  return (
     <ThemeProvider theme={theme}>
       <Container component="main" maxWidth="xs">
         <CssBaseline />
         <Box
           sx={{
             marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
             <LockOutlinedIcon />
           </Avatar>
           <Typography component="h1" variant="h5">
             Sign up
           </Typography>
-          <Box component="form" noValidate onSubmit={handleChange} sx={{ mt: 3 }}>
+          <Box
+            component="form"
+            noValidate
+            onSubmit={handleChange}
+            sx={{ mt: 3 }}
+          >
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
@@ -139,12 +133,6 @@ export default function Signup(props) {
                   autoComplete="new-password"
                 />
               </Grid>
-              {/* <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid> */}
             </Grid>
             <Button
               type="submit"
@@ -157,56 +145,15 @@ export default function Signup(props) {
             </Button>
             <Grid container justifyContent="flex-end">
               <Grid item>
-                <Link href="/users/signup" variant="body2">
+                <Link href="/users/login" variant="body2">
                   Already have an account? Sign in
                 </Link>
               </Grid>
             </Grid>
           </Box>
         </Box>
-        <Copyright sx={{ mt: 5 }} />
+        <Copyright />
       </Container>
     </ThemeProvider>
   );
-
-  // return (
-  //   <form>
-  //     <label htmlFor="firstname">First Name</label>
-  //     <input
-  //       type="text"
-  //       name="firstname"
-  //       id="firstname"
-  //       value={person.firstname}
-  //       onChange={handleChange}
-  //     />
-  //     <label htmlFor="lastname">Last Name</label>
-  //     <input
-  //       type="text"
-  //       name="lastname"
-  //       id="lastname"
-  //       value={person.lastname}
-  //       onChange={handleChange}
-  //     />
-  //     <label htmlFor="email">Email</label>
-  //     <input
-  //       type="text"
-  //       name="email"
-  //       id="email"
-  //       value={person.email}
-  //       onChange={handleChange}
-  //     />
-  //     <label htmlFor="password">Password</label>
-  //     <input
-  //       type="password"
-  //       name="password"
-  //       id="password"
-  //       value={person.password}
-  //       onChange={handleChange}
-  //     />
-  //     <input type="button" value="Submit" onClick={submitForm} />
-  //     <li>
-  //       <a href="/">Go Back</a>
-  //     </li>
-  //   </form>
-  // );
 }
