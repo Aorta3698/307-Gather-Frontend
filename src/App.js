03383cd5797b26e0d6b99dcc69af8b8196041e8e
abentler <abentler@calpoly.edu>
2023-03-14 10:04:09 -0700

    Saving the favorites page for after merge

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Welcome from "./Welcome";
 import Login from "./Login";
 import Signup from "./Signup";
 import Profile from "./Profile";
-import Favorites from "./Favorites"
 import { checkCredentials } from "./Requests.js";
 import { addAccount } from "./Requests.js";
 
@@ -15,7 +14,6 @@ export default function App() {
       <Route path="/" element={<Welcome />} />
       <Route path="/Home" element={<Home />} />
       <Route path="/Profile" element={<Profile />} />
-      <Route path="/Favorites" element={<Favorites />} />
       <Route path="/users/Login" element={<Login handleSubmit={checkCredentials}/>} />
       <Route path="/users/signup" element={<Signup handleSubmit={addAccount}/>} />
     </Routes>
