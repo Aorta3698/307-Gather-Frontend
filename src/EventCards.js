dc970d9e891f25b2c39baca96b843b3fa1b5e8f2
anarchaworld <anarchaworld@gmail.com>
2023-03-13 19:05:35 -0700

    verified icon added

--- src/EventCards.js
+++ src/EventCards.js
@@ -6,6 +6,13 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import VerifiedIcon from "@mui/icons-material/Verified";
+
+function isVerified(event) {
+  if (event.verified) {
+    return <VerifiedIcon />;
+  }
+}
 
 export default function EventCards(props) {
   return (
@@ -31,7 +38,7 @@ export default function EventCards(props) {
               />
               <CardContent sx={{ flexGrow: 1 }}>
                 <Typography gutterBottom variant="h5" component="h2">
-                  {event.name}
+                  {event.name} {isVerified(event)}
                 </Typography>
                 <Typography>{event.description}</Typography>
               </CardContent>
