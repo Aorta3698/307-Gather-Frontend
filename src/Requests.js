41ed1a7d5dfab1afff40e65d070629cdb68cf0ea
anarchaworld <anarchaworld@gmail.com>
2023-03-14 21:34:36 -0700

    url to deployment

--- src/Requests.js
+++ src/Requests.js
@@ -1,39 +1,36 @@
-import axios from 'axios';
+import axios from "axios";
 
-async function makeAccountPostCall(person){
-   try {
-   const response = await axios.post('http://localhost:5000/users', person);
-   return response;
-   }
-   catch (error) {
-   console.log(error);
-   return false;
-   }
+const backend = "https://polygather.azurewebsites.net/";
+
+async function makeAccountPostCall(person) {
+  try {
+    const response = await axios.post(backend + "users", person);
+    return response;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
 export function addAccount(person) {
-   makeAccountPostCall(person).then( result => {
-   if (result && result.status === 201)
-      console.log('CreateAccount: Success!');
-   });  
+  makeAccountPostCall(person).then((result) => {
+    if (result && result.status === 201) console.log("CreateAccount: Success!");
+  });
 }
 
-async function makeLoginPostCall(person){
-   try {
-   const response = await axios.post('http://localhost:5000/login', person);
-   return response;
-   }
-   catch (error) {
-   console.log(error);
-   return false;
-   }
+async function makeLoginPostCall(person) {
+  try {
+    const response = await axios.post(backend + "login", person);
+    return response;
+  } catch (error) {
+    console.log(error);
+    return false;
+  }
 }
 
 export function checkCredentials(person) {
-   makeLoginPostCall(person).then( result => {
-   if (result && result.status === 200)
-      console.log('Login: Success!');
-   else
-      console.log('Login: Failure.');
-   });  
+  makeLoginPostCall(person).then((result) => {
+    if (result && result.status === 200) console.log("Login: Success!");
+    else console.log("Login: Failure.");
+  });
 }
