c7afa8ce797e79929553e4f37d389edae3cfce43
anarchaworld <anarchaworld@gmail.com>
2023-03-16 19:42:45 -0700

    fixed a missing {} in tab

--- src/Tab.js
+++ src/Tab.js
@@ -1,19 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Button from "@mui/material/Button";
 
-export default function Tab(path, label) {
+export default function Tab({ path, label }) {
   return (
-    <Button>
-      <NavLink
-        to={path}
-        end
-        className={({ isActive }) =>
-          "nav-bar__tab " + (isActive ? "nav-bar__tab--active" : "")
-        }
-      >
-        {label}
-      </NavLink>
-    </Button>
+    <NavLink
+      to={path}
+      end
+      className={({ isActive }) =>
+        "nav-bar__tab " + (isActive ? "nav-bar__tab--active" : "")
+      }
+    >
+      {label}
+    </NavLink>
   );
 }
