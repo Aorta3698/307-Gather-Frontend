2b52e77e7851d1986c2d4ece4f10ee014b0fa2c3
anarchaworld <anarchaworld@gmail.com>
2023-03-16 17:19:53 -0700

    added guard

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Profile from "./Profile";
 import ResetPass from "./ResetPass";
 import EventNew from "./EventNew";
 import PageLoader from "./PageLoader";
+import { AuthenticationGuard } from "./authentication-guard";
 
 export default function App() {
   const { isLoading } = useAuth0();
@@ -23,11 +24,16 @@ export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/Profile" element={<Profile />} />
+      <Route
+        path="/Profile"
+        element={<AuthenticationGuard component={Profile} />}
+      />
       <Route path="/users/Login" element={<Login />} />
       <Route path="/users/signup" element={<Signup />} />
-      <Route path="/users/reset-pass" element={<ResetPass />} />
-      <Route path="/events/new" element={<EventNew />} />
+      <Route
+        path="/events/new"
+        element={<AuthenticationGuard component={EventNew} />}
+      />
     </Routes>
   );
 }
